21e7ddbefaca03fdf92aa6622ec9de096ac127fa
xcymo <361855254@qq.com>
2019-06-17 22:55:00 +0800

    单变量线性回归、梯度下降作业

--- computeCost.m
+++ computeCost.m
@@ -8,7 +8,9 @@ m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
 J = 0;
-
+hy = X*theta;
+sumhy=(hy-y).^2/(2*m);
+J = sum(sumhy);
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
--- gradientDescent.m
+++ gradientDescent.m
@@ -6,9 +6,7 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
-
 for iter = 1:num_iters
-
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
     %               theta. 
@@ -16,18 +14,13 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
-
+    % for i=1:length(theta)
+    %     theta(i) = theta(i) - X(:,i)'*(X*theta-y)*alpha/m;
+    % end
+    theta=theta-X'*(X*theta-y)*alpha/m
     % ============================================================
-
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
 
 end
-
 end
